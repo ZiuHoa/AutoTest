d04c23cfa7de5bebf6e3487fa314c8de1a979091
ZiuHoa <duyhoa.ic29@gmail.com>
2024-12-09 20:31:52 +0700

    Bai1

---
+++ src/test/java/Demo.java
@@ -0,0 +1,34 @@
+import UI.LoginDemoUI;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class Demo {
+    private  WebDriver driver;
+    private LoginDemoUI loginDemoUI;
+    public Demo(WebDriver driver) {
+        this.driver = driver;
+        this.loginDemoUI = new LoginDemoUI(driver);
+    }
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://saucelabs.com/request-demo");
+        // Tạo instance của TestDemo
+        Demo Demo = new Demo(driver);
+        // Sử dụng requestDemoPageUI
+        Demo.loginDemoUI.findInputEmail().sendKeys("duyhoa.ic29@gmail.com");
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        Demo.loginDemoUI.findInputFirstName().sendKeys("FirstName");
+        Demo.loginDemoUI.findInputLastName().sendKeys("LastName");
+        Demo.loginDemoUI.findInputCompany().sendKeys("ABCompany");
+        Demo.loginDemoUI.findInputPhoneNumber().sendKeys("+84123456789");
+        Demo.loginDemoUI.findCountry();
+        Demo.loginDemoUI.findInterest();
+        Demo.loginDemoUI.findInputComments().sendKeys("Comments");
+        Demo.loginDemoUI.findCheckboxPolicy().click();
+        Demo.loginDemoUI.findButtonLetsTalk().click();
+        driver.quit();
+    }
+}
