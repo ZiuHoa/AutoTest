02724b6548b8395cc8ed3a729f494fd00edb7403
ZiuHoa <duyhoa.ic29@gmail.com>
2024-12-10 21:00:21 +0700

    Bai2

--- src/test/java/Demo.java
+++ src/test/java/Demo.java
@@ -14,34 +14,52 @@ public class Demo {
         this.driver = driver;
         this.loginDemoUI = new LoginDemoUI(driver);
     }
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
-        driver.get("https://saucelabs.com/request-demo");
+        driver.get("https://demo.guru99.com/test/upload/");
         //driver.navigate().to("https://saucelabs.com/request-demo");
         //In đường dẫn
         //String getUrl = driver.getCurrentUrl();
         //System.out.println("Đường dẫn là: " + getUrl);
+        //Đường dẫn cần tải lên:
+        String filePath = "D:\\Devpro\\Auto test\\Product\\autoTest\\target\\test-classes\\Demo.class";
 
-        //Tìm và in text
-        WebElement header = driver.findElement(By.tagName("h1"));
-        System.out.println("Header text: " + header.getText());
+        //Tìm phần tử input type="file" và gửi đường dẫn tệp
+        WebElement uploadFile = driver.findElement(By.id("uploadfile_0"));
+        uploadFile.sendKeys(filePath);
 
-        //Tìm list và in text
-        List<WebElement> paragraphs = driver.findElements(By.tagName("p"));
-        System.out.println("\nParagraphs:");
-        for (WebElement paragraph : paragraphs) {
-            System.out.println(paragraph.getText());
-        }
-        //Tìm phần tử theo CSS Selector: tất cả các nút <button>
-        List<WebElement> buttons = driver.findElements(By.cssSelector("button"));
-        System.out.println("\nButtons:");
-        for (WebElement button : buttons) {
-            System.out.println(button.getText());
-        }
-        //Tìm phần tử theo CSS Selector: tất ả các input có name là "Email"
-        WebElement emailInput = driver.findElement(By.cssSelector("input[name='Email']"));
-        emailInput.sendKeys("abc@gmail.com");
+        WebElement termsCheckbox = driver.findElement(By.id("terms"));
+        termsCheckbox.click();
+
+        WebElement uploadButton = driver.findElement(By.id("submitbutton"));
+        uploadButton.click();
+
+        Thread.sleep(5000);
+
+        WebElement resultsMessage = driver.findElement(By.id("res"));
+        System.out.println("Ket qua la:" + resultsMessage.getText());
+
+
+//        //Tìm và in text
+//        WebElement header = driver.findElement(By.tagName("h1"));
+//        System.out.println("Header text: " + header.getText());
+//
+//        //Tìm list và in text
+//        List<WebElement> paragraphs = driver.findElements(By.tagName("p"));
+//        System.out.println("\nParagraphs:");
+//        for (WebElement paragraph : paragraphs) {
+//            System.out.println(paragraph.getText());
+//        }
+//        //Tìm phần tử theo CSS Selector: tất cả các nút <button>
+//        List<WebElement> buttons = driver.findElements(By.cssSelector("button"));
+//        System.out.println("\nButtons:");
+//        for (WebElement button : buttons) {
+//            System.out.println(button.getText());
+//        }
+//        //Tìm phần tử theo CSS Selector: tất ả các input có name là "Email"
+//        WebElement emailInput = driver.findElement(By.cssSelector("input[name='Email']"));
+//        emailInput.sendKeys("abc@gmail.com");
 //        Một số CSS Selector phổ biến:
 //        tagName.className:
 //        Tìm thẻ với class cụ thể. Ví dụ: button.submit (tìm thẻ <button> có class là submit).
@@ -58,7 +76,7 @@ public class Demo {
 //                tagName:first-child:
 //        Tìm phần tử con đầu tiên. Ví dụ: li:first-child.
         // Tạo instance của TestDemo
-        Demo Demo = new Demo(driver);
+        //Demo Demo = new Demo(driver);
         // Sử dụng requestDemoPageUI
         /*Demo.loginDemoUI.findInputEmail().sendKeys("duyhoa.ic29@gmail.com");
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
